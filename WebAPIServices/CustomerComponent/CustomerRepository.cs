a73cf9e15d21eae97c2f642479b6d733aa040d25
Jude <amozu2008@gmail.com>
2022-05-07 12:57:16 +0100

    Add project files.

---
+++ WebAPIServices/CustomerComponent/CustomerRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIServices.DomainModel;
+using WebAPIServices.Dto;
+
+namespace WebAPIServices.CustomerComponent
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        protected readonly IMapper _mapper;
+        public static List<Customer> CustomerDB = new List<Customer>
+        {
+            new Customer{CustomerId = 1,Name = "Jude",Surname = "Kc",InitialCredit = 0},
+            new Customer{CustomerId = 2,Name = "Olivia",Surname = "Tim",InitialCredit = 0},
+            new Customer{CustomerId = 3,Name = "Michael",Surname = "Kelechi",InitialCredit = 0}
+        };
+
+        public CustomerRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public CustomerDto AddCustomer(CustomerDto customerDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerDto FindCustomerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CustomerDto> GetAllCustomer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
