e54e4123df91b4f2e6624cbfe2cd040371b8106e
Jude <amozu2008@gmail.com>
2022-05-08 15:45:22 +0100

    modified

--- FrontEndWeb/wwwroot/scripts/HTTPClass.js
+++ FrontEndWeb/wwwroot/scripts/HTTPClass.js
@@ -16,7 +16,7 @@
     }
 
     async getAllCustomer(url) {
-        const reponse = await fetch(url);
+        const response = await fetch(url);
         const responseData = await response.json();
 
         return responseData;
