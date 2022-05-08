ec5fc2448e8b673f0227af652f262f3113d0da08
Jude <amozu2008@gmail.com>
2022-05-08 15:37:55 +0100

    modified

--- FrontEndWeb/wwwroot/scripts/consumeApi.js
+++ FrontEndWeb/wwwroot/scripts/consumeApi.js
@@ -8,48 +8,49 @@ const listData = document.querySelector("#listDataId");
 const allCustomerBtn = document.querySelector("#allCustomerBtn");
 
 
-createBtn.addEventListener("click", () => {
-    let customerValue = customerTxt.value;
-    let creditValue = initialCreditTxt.value;
+//createBtn.addEventListener("click", () => {
+//    let customerValue = customerTxt.value;
+//    let creditValue = initialCreditTxt.value;
 
-    if (customerValue == "" || creditValue == "") {
+//    if (customerValue == "" || creditValue == "") {
 
-        alertBox.innerHTML = `Please enter all fields`;
-        alertBox.classList.remove("alert-success");
-        alertBox.classList.add("alert-danger");
-        alertBox.classList.replace("d-none", "d-block");
+//        alertBox.innerHTML = `Please enter all fields`;
+//        alertBox.classList.remove("alert-success");
+//        alertBox.classList.add("alert-danger");
+//        alertBox.classList.replace("d-none", "d-block");
 
-    }
-    else {
-        alertBox.classList.replace("d-block", "d-none");
-        const customerObj = {
-            customerId : customerValue,
-            initialCredit : creditValue
-        }
-        ConsumeHttp.registerUsers("https://localhost:44395/api/account", customerObj)
-            .then(data => {
-                                if (data.isSuccess) {
+//    }
+//    else {
+//        alertBox.classList.replace("d-block", "d-none");
+//        const customerObj = {
+//            customerId : customerValue,
+//            initialCredit : creditValue
+//        }
+//        ConsumeHttp.registerUsers("https://localhost:44395/api/account", customerObj)
+//            .then(data => {
+//                                if (data.isSuccess) {
 
-                                    alertBox.innerHTML = data.displayMessages;
-                                    alertBox.classList.remove("alert-danger");
-                                    alertBox.classList.add("alert-success");
-                                    alertBox.classList.replace("d-none", "d-block");
-                                    customerValue == "";
-                                    creditValue == ""
-                                }
-                                else {
-                                    alertBox.innerHTML = data.displayMessages;
-                                    alertBox.classList.remove("alert-success");
-                                    alertBox.classList.add("alert-danger");
-                                    alertBox.classList.replace("d-none", "d-block");
-                                }
-                           })
-            .catch(err => console.log(err));
-    }
+//                                    alertBox.innerHTML = data.displayMessages;
+//                                    alertBox.classList.remove("alert-danger");
+//                                    alertBox.classList.add("alert-success");
+//                                    alertBox.classList.replace("d-none", "d-block");
+//                                    customerValue == "";
+//                                    creditValue == ""
+//                                }
+//                                else {
+//                                    alertBox.innerHTML = data.displayMessages;
+//                                    alertBox.classList.remove("alert-success");
+//                                    alertBox.classList.add("alert-danger");
+//                                    alertBox.classList.replace("d-none", "d-block");
+//                                }
+//                           })
+//            .catch(err => console.log(err));
+//    }
 
-});
+//});
 
 allCustomerBtn.addEventListener("click", () => {
+    alert("Hey")
     ConsumeHttp.getAllCustomer("https://localhost:44395/api/account")
         .then(data => {
             let outputData = "";
@@ -66,4 +67,5 @@ allCustomerBtn.addEventListener("click", () => {
             listData.innerHTML = outputData;
 
         })
-})
+        .catch(err => console.log(err));
+});
