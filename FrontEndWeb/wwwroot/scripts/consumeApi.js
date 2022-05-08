793a59b94087166f6715bc89a217d79c3bbc7c3c
Jude <amozu2008@gmail.com>
2022-05-08 11:54:48 +0100

    implement consumeapi.js on the front end web app

---
+++ FrontEndWeb/wwwroot/scripts/consumeApi.js
@@ -0,0 +1,30 @@
+﻿const ConsumeHttp = new HTTPClass;
+
+const customerTxt = document.querySelector("#customerIdTxt");
+const initialCreditTxt = document.querySelector("#initialCreditTxt");
+const createBtn = document.querySelector("#btnid");
+const alertBox = document.querySelector("#alertBoxId");
+
+createBtn.addEventListener("click", () => {
+    let customerValue = customerTxt.value;
+    let creditValue = initialCreditTxt.value;
+
+    if (customerValue == "" || creditValue == "") {
+
+        alertBox.innerHTML = `Please enter all fields`;
+        alertBox.classList.remove("alert-success");
+        alertBox.classList.add("alert-danger");
+        alertBox.classList.replace("d-none", "d-block");
+
+    }
+    else {
+        customerObj = {
+            customerId = customerValue,
+            initialCredit = creditValue
+        }
+        ConsumeHttp.registerUsers("https://localhost:44395/api/account", customerObj)
+            .then(customerObj => console.log(customerObj))
+            .catch(err => console.log(err));
+    }
+
+});
