e851b1915943282c6e7db1702433a3bf4d129f5b
Jude <amozu2008@gmail.com>
2022-05-08 16:01:39 +0100

    modified

--- FrontEndWeb/wwwroot/scripts/consumeApi.js
+++ FrontEndWeb/wwwroot/scripts/consumeApi.js
@@ -8,60 +8,60 @@ const listData = document.querySelector("#listDataId");
 const allCustomerBtn = document.querySelector("#allCustomerBtn");
 
 
-//createBtn.addEventListener("click", () => {
-//    let customerValue = customerTxt.value;
-//    let creditValue = initialCreditTxt.value;
+createBtn.addEventListener("click", () => {
+    let customerValue = customerTxt.value;
+    let creditValue = initialCreditTxt.value;
 
-//    if (customerValue == "" || creditValue == "") {
+    if (customerValue == "" || creditValue == "") {
 
-//        alertBox.innerHTML = `Please enter all fields`;
-//        alertBox.classList.remove("alert-success");
-//        alertBox.classList.add("alert-danger");
-//        alertBox.classList.replace("d-none", "d-block");
+        alertBox.innerHTML = `Please enter all fields`;
+        alertBox.classList.remove("alert-success");
+        alertBox.classList.add("alert-danger");
+        alertBox.classList.replace("d-none", "d-block");
 
-//    }
-//    else {
-//        alertBox.classList.replace("d-block", "d-none");
-//        const customerObj = {
-//            customerId : customerValue,
-//            initialCredit : creditValue
-//        }
-//        ConsumeHttp.registerUsers("https://localhost:44395/api/account", customerObj)
-//            .then(data => {
-//                                if (data.isSuccess) {
+    }
+    else {
+        alertBox.classList.replace("d-block", "d-none");
+        const customerObj = {
+            customerId : customerValue,
+            initialCredit : creditValue
+        }
+        ConsumeHttp.registerUsers("https://localhost:44395/api/account", customerObj)
+            .then(data => {
+                                if (data.isSuccess) {
 
-//                                    alertBox.innerHTML = data.displayMessages;
-//                                    alertBox.classList.remove("alert-danger");
-//                                    alertBox.classList.add("alert-success");
-//                                    alertBox.classList.replace("d-none", "d-block");
-//                                    customerValue == "";
-//                                    creditValue == ""
-//                                }
-//                                else {
-//                                    alertBox.innerHTML = data.displayMessages;
-//                                    alertBox.classList.remove("alert-success");
-//                                    alertBox.classList.add("alert-danger");
-//                                    alertBox.classList.replace("d-none", "d-block");
-//                                }
-//                           })
-//            .catch(err => console.log(err));
-//    }
+                                    alertBox.innerHTML = data.displayMessages;
+                                    alertBox.classList.remove("alert-danger");
+                                    alertBox.classList.add("alert-success");
+                                    alertBox.classList.replace("d-none", "d-block");
+                                    customerValue = "";
+                                    creditValue = ""
+                                }
+                                else {
+                                    alertBox.innerHTML = data.displayMessages;
+                                    alertBox.classList.remove("alert-success");
+                                    alertBox.classList.add("alert-danger");
+                                    alertBox.classList.replace("d-none", "d-block");
+                                }
+                           })
+            .catch(err => console.log(err));
+    }
 
-//});
+});
 
 allCustomerBtn.addEventListener("click", () => {
-    alert("Hey")
+   // alert("Hey")
     ConsumeHttp.getAllCustomer("https://localhost:44395/api/account")
         .then(data => {
             let outputData = "";
-            data.forEach(post => {
+            data.forEach(item => {
                 outputData += `<a href="#" class="list-group-item list-group-item-action">
                                     <div class="d-flex w-100 justify-content-between">
-                                        <h5 class="mb-1">${data.customer.name} ${data.customer.surname}</h5>
-                                        <small>initialCredit ${data.customer.initialCredit}</small>
+                                        <h5 class="mb-1">${item.customer.name} ${item.customer.surname}</h5>
+                                        <small>initialCredit ${item.customer.initialCredit}</small>
                                     </div>
-                                    <p class="mb-1">Balance ${data.balance}</p>
-                                    <small>Transaction ${data.transactions}</small>
+                                    <p class="mb-1">Balance ${item.balance}</p>
+                                    <small>Transaction ${item.transactions}</small>
                             </a>`
             });
             listData.innerHTML = outputData;
